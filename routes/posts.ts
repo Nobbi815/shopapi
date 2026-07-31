e361a013d054a7bfa20349d02c68a777efbea766
SawHlaOo <sawhlaooroguishx@gmail.com>
2026-07-31 22:39:18 +0630

    fifth commit

--- routes/posts.ts
+++ routes/posts.ts
@@ -1,25 +1,24 @@
 import express from "express";
-import {prisma} from '../lib/prisma';
-import {success, z}from "zod";
-import { error } from "node:console";
-export const router=express.Router()
+import { prisma, Prisma } from '../lib/prisma';
+import { z } from "zod";
+
+export const router = express.Router();
 
 // postApi for games
 router.post("/games", async (req, res) => {
-    
   try {
     const gameSchema = z.object({
       name: z.string().trim().min(1, "Name is required"),
       description: z.string().trim().optional(),
-      image: z.string().trim().pipe(z.url()),
-      logo: z.string().trim().pipe(z.url()),
-      badge: z.string().trim().pipe(z.url()),
+      image: z.string().trim().url(),
+      logo: z.string().trim().url(),
+      badge: z.string().trim().optional(),
     });
 
     const gamedata = gameSchema.parse(req.body);
 
     const game = await prisma.game.create({
-      data: gamedata,
+      data: gamedata as Prisma.GameCreateInput,
     });
 
     return res.status(201).json({
@@ -34,66 +33,59 @@ router.post("/games", async (req, res) => {
   }
 });
 
-router.post("/apps", async(req,res)=>{
-    try{
-        const appSchema=z.object({
-            name:z.string().trim().min(1,"Appname is required"),
-            descrition:z.string().trim().optional(),
-            image:z.string().trim().pipe(z.url()),
-            badge:z.string().trim().optional(),
-            logo:z.string().trim().pipe(z.url())
-        })
-        const appdata= appSchema.parse(req.body);
+router.post("/apps", async (req, res) => {
+  try {
+    const appSchema = z.object({
+      name: z.string().trim().min(1, "Appname is required"),
+      description: z.string().trim().optional(),
+      image: z.string().trim().url(),
+      badge: z.string().trim().optional(),
+      logo: z.string().trim().url(),
+    });
+    const appdata = appSchema.parse(req.body);
 
-        const app= await prisma.app.create({
-            data:appdata
-        })
+    const app = await prisma.app.create({
+      data: appdata as Prisma.AppCreateInput,
+    });
 
-        return res.status(201).json({
-            success:true,
-            data:app
-        })
-    }
-    
-    catch (error){
-        return res.status(400).json({
-            success:false,
-            error
-        })
+    return res.status(201).json({
+      success: true,
+      data: app,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      error,
+    });
+  }
+});
 
-    }
-})
+router.post("/powerpoints", async (req, res) => {
+  try {
+    const powerpointSchema = z.object({
+      name: z.string().trim().min(1, "Appname is required"),
+      description: z.string().trim().optional(),
+      image: z.string().trim().url(),
+      badge: z.string().trim().optional(),
+      logo: z.string().trim().url(),
+    });
+    const powerpointData = powerpointSchema.parse(req.body);
 
-router.post("/powerpoints"
-    , async(req,res)=>{
-    try{
-        const appSchema=z.object({
-            name:z.string().trim().min(1,"Appname is required"),
-            descrition:z.string().trim().optional(),
-            image:z.string().trim().pipe(z.url()),
-            badge:z.string().trim().optional(),
-            logo:z.string().trim().pipe(z.url())
-        })
-        const appdata= appSchema.parse(req.body);
+    const powerpoint = await prisma.powerpoint.create({
+      data: powerpointData as Prisma.PowerpointCreateInput,
+    });
 
-        const app= await prisma.app.create({
-            data:appdata
-        })
-
-        return res.status(201).json({
-            success:true,
-            data:app
-        })
-    }
-    
-    catch (error){
-        return res.status(400).json({
-            success:false,
-            error
-        })
-
-    }
-})
+    return res.status(201).json({
+      success: true,
+      data: powerpoint,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      error,
+    });
+  }
+});
 
 router.get("/games", async (req,res)=>{
     
