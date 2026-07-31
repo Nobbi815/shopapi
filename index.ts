bddf75ffd8f7d952dd401e0f645aa1e515d44b1a
SawHlaOo <sawhlaooroguishx@gmail.com>
2026-07-31 22:13:07 +0630

    third commit

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import helmet from "helmet";
 import { connectRedis } from "./lib/redis";
 import { router as usersRouter } from "./routes/users";
 import { router as productsRouter } from "./routes/products";
@@ -13,7 +12,12 @@ const app = express();
 const port = Number(process.env.PORT || 8800);
 
 app.disable("x-powered-by");
-app.use(helmet());
+app.use((_req, res, next) => {
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.setHeader("X-Frame-Options", "SAMEORIGIN");
+  res.setHeader("Referrer-Policy", "no-referrer");
+  next();
+});
 app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
